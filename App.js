364c15fd0508ecdcd15eeaee3479f08df2407e2a
Joshe <joseinformatico2015@gmail.com>
2020-04-20 17:59:19 -0400

    antes de gps

--- App.js
+++ App.js
@@ -2,42 +2,43 @@
 
 
 import React, { Component } from "react";
-import { StyleSheet, View, Dimensions } from "react-native";
+import { StyleSheet, View, Dimensions, Text, Button, TouchableHighlight, TouchableOpacity} from "react-native";
 import MapboxGL from "@react-native-mapbox-gl/maps";
 import {featureCollection, feature} from '@turf/turf';
-
+import Bubble from './Bubble';
 MapboxGL.setAccessToken("pk.eyJ1Ijoiam9zZWluZm9ybWF0aWNvMjAxNSIsImEiOiJjazdzNWFmYnIwY21uM3NvNHZoemg5ZmJqIn0.EHg5K9m_gdHJBnL1JcudYg");
 
 
 // import userIcon from '../assets/example.png';
 import otherUserIcon from './assets/image/userIcon.png';
+import userIcon from './assets/image/userThis.png';
+
+var markers=[
+  [-73.970895, 40.723279],
+  [-74.0037382718885,40.97053006323466],
+  [-74.037382718885,41],
+
+]
+
+var puntos=markers.map((marker)=>(
+    {
+      type:"Feature",
+      properties:{icon:"myIcon"},
+      geometry:{type:"Point",coordinates:marker}
+    }
+    )
+  )
 
 const featureCollections = {
   type: 'FeatureCollection',
-  features: [
-      {
-          type: 'Feature',
-          properties: {
-              icon: 'myIcon',
-          },
-          geometry: {
-              type: 'Point',
-              coordinates: [-73.970895, 40.723279]
-          },
-      },
-      {
-        type: 'Feature',
-        properties: {
-            icon: 'myIcon',
-        },
-        geometry: {
-            type: 'Point',
-            coordinates: [-73.979895, 40.729279]
-        },
-      },
-  ],
+  features: puntos,
 };
 
+const userFeature = {
+  type:"Feature",
+  properties:{icon:"myIcon"},
+  geometry:{type:"Point",coordinates:  [-74.03582718885,41.5]}
+}
 
 const stylesMap ={
   icon: {
@@ -45,6 +46,11 @@ const stylesMap ={
     iconAllowOverlap: true,
     iconSize:0.08
   },
+  iconUser: {
+    iconImage: userIcon,
+    iconAllowOverlap: true,
+    iconSize:0.08
+  }
 };
 
 let { width, height } = Dimensions.get('window');
@@ -58,42 +64,88 @@ export default class App extends Component {
       // featureCollection: featureCollection([
       // ]),
       featureCollection:featureCollections,
-
+    
+    
     };
 
     this.onPress=this.onPress.bind(this);
-
+    this.onPress=this.onPress.bind(this);
   }
 
   componentDidMount() {
     MapboxGL.setTelemetryEnabled(false);
-    // console.log(this.state.featureCollection)
+   // console.log(this.state.featureCollection)
     // this.setState({
     //   featureCollection: featureCollection([
     //     ...this.state.featureCollection.features,
     //     feature({"geometry":{"coordinates":[-74.08796,40.6998],"type":"Point"}})
     //   ]),
     // });
-    console.log(this.state.featureCollection)
+    // console.log("feature collections"+JSON.stringify(this.state.featureCollection))
+    // console.log("puntos"+JSON.stringify(puntos))
   }
-  onSourceLayerPress(e) {
-    const feature = e.nativeEvent.payload;
-    console.log('You pressed a layer here is your feature', feature); // eslint-disable-line
-  }
+  onSourceLayerPress(event) {
+    // console.log(event)
 
-  async onPress(e) {
-    console.log(e);
-    const aFeature = feature(e.geometry);
-    console.log(aFeature);
-    aFeature.id = `${Date.now()}`;
-
+    const feature = event
+    // console.log('JSON feature', JSON.stringify(feature)); // eslint-disable-line
+    // console.log('You pressed a layer here is your feature', feature); // eslint-disable-line
+  
     this.setState({
-      featureCollection: featureCollection([
-        ...this.state.featureCollection.features,
-        aFeature,
-      ]),
+      latitude: event.coordinates.latitude,
+      longitude: event.coordinates.longitude,
+      screenPointX: event.point.x,
+      screenPointY: event.point.y,
     });
+  }
 
+
+  get hasValidLastClick() {
+    return (
+      typeof this.state.latitude === 'number' &&
+      typeof this.state.longitude === 'number'
+    );
+  }
+
+  renderLastClicked() {
+    if (!this.hasValidLastClick) {
+      // return (
+      //   <Bubble>
+      //     <Text>Click the map!</Text>
+      //   </Bubble>
+      // );
+      return null
+    }
+
+    return (
+      <Bubble>
+        <Text>Latitude: {this.state.latitude}</Text>
+        <Text>Longitude: {this.state.longitude}</Text>
+        <Text>Screen Point X: {this.state.screenPointX}</Text>
+        <Text>Screen Point Y: {this.state.screenPointY}</Text>
+        <TouchableHighlight
+            style={styles.submit}
+            onPress={() => {}}
+            underlayColor='#fff'>
+            <Text style={[styles.submitText]}>Ir en Ayuda</Text>
+          </TouchableHighlight>
+      </Bubble>
+    );
+  }
+  async onPress(e) {
+    // console.log(e);
+    // const aFeature = feature(e.geometry);
+    // console.log(aFeature);
+    // aFeature.id = `${Date.now()}`;
+
+    // this.setState({
+    //   featureCollection: featureCollection([
+    //     ...this.state.featureCollection.features,
+    //     aFeature,
+    //   ]),
+    // });
+    // console.log(JSON.stringify(e))
+   
   }
 
   render() {
@@ -115,19 +167,32 @@ export default class App extends Component {
               id='exampleShapeSource'
               hitbox={{width: 20, height: 20}}
                shape={this.state.featureCollection}
-              // onPress={(feature) => this.onShapeSourceLayer(feature)}
+               onPress={(feature) => this.onSourceLayerPress(feature)}
               >
               
               <MapboxGL.SymbolLayer 
                 id='exampleIconName' 
                 minZoomLevel={1} 
                 style={stylesMap.icon}/>
-            
+          
+            </MapboxGL.ShapeSource>
+
+            <MapboxGL.ShapeSource
+              id='userIcon'
+              hitbox={{width: 20, height: 20}}
+               shape={userFeature}
+               onPress={(feature) => this.onSourceLayerPress(feature)}>
+              
+              <MapboxGL.SymbolLayer 
+                id='userIcon' 
+                minZoomLevel={1} 
+                style={stylesMap.iconUser}/>
+          
             </MapboxGL.ShapeSource>
 
           </MapboxGL.MapView>
 
-
+          {this.renderLastClicked()}
         </View>
       </View>
     );
@@ -148,6 +213,21 @@ const styles = StyleSheet.create({
   },
   map: {
     flex: 1
+  },
+  submit:{
+    marginRight:40,
+    marginLeft:40,
+    marginTop:10,
+paddingHorizontal:15,
+paddingVertical:10,
+    backgroundColor:'#68a0cf',
+    borderRadius:10,
+    borderWidth: 1,
+    borderColor: '#fff'
+  },
+  submitText:{
+      color:'#fff',
+      textAlign:'center',
   }
 });
 
